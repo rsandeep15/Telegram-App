8f4ccab33e3ea1300603fd4fff1e8befbb3c2ebc
rsandeep15 <frostbitten.store@gmail.com>
2014-07-17 00:18:34 -0700

    Initial Commit

---
+++ Telegram App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Telegram App
+//
+//  Created by admin on 7/17/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SRAppDelegate class]));
+    }
+}
